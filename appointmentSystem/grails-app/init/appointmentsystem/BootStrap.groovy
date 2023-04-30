ffdf97aab7f329f1dbe165fb0ab74a5e045314d1
c1007012 <student@lab000000.1cyzc2seaqrubg5u0cu41e02ah.zx.internal.cloudapp.net>
2023-04-30 11:09:54 +0000

    Updated Database and Bootstrap

--- appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -3,7 +3,7 @@ package appointmentsystem
 class BootStrap {
 
     def init = { servletContext ->
-        /*def surgery = new Surgery(
+        def surgery = new Surgery(
 	 name: 'Sheffield Medical Centre',
          address: '21 Spital Street, Sheffield',
          postcode: 'S3 9LB',
@@ -11,6 +11,17 @@ class BootStrap {
          numberOfPatients: 80,
          description: 'Lorem ipsum',
          openingTime: '09:00 - 19:00',
+         registeringNewPatients: true
+        ).save()
+
+        def surgery2 = new Surgery(
+	 name: 'Manchester Road Surgery',
+         address: '484 Manchester Road, Sheffield',
+         postcode: 'S10 5PN',
+         telephone: '0114 266 8265',
+         numberOfPatients: 105,
+         description: 'Lorem ipsum',
+         openingTime: '08:00 - 17:00',
          registeringNewPatients: true
         ).save()
 	
@@ -26,6 +37,18 @@ class BootStrap {
          surgery: surgery  
         ).save()
 
+        def doctor2 = new Doctor(
+         doctorName: 'Darwin Paulson',
+         qualification: 'BMBS',
+         position: 'Junior Doctor',
+         doctorEmail: 'd.paulson@outlook.com',
+         password: 'password',
+         doctorOffice: 'D-13',
+         doctorPhone: '07376112349',
+         bio: 'Lorem Ipsum',
+         surgery: surgery  
+        ).save()
+
         def patient = new Patient(
          patientName: 'John Doe',
          patientAddress: '23 Prospect Avenue',
@@ -37,12 +60,33 @@ class BootStrap {
          surgery: surgery
         ).save()
 
+        def patient2 = new Patient(
+         patientName: 'Juliane Johnson',
+         patientAddress: '12  Fairfax Road',
+         patientResidence: 'Sheffield',
+         patientDob: '1987-04-19',
+         patientID: 'P5438',
+         dateRegistered: '2023-02-09',
+         patientPhone: '07932665178',
+         surgery: surgery
+        ).save()
+
         def receptionist = new Receptionist(
-         recepName: 'Daniel Jones',
-         recepEmail: 'danieljones@outlook.com',
+         recepName: 'Daniel Stephens',
+         recepEmail: 'danielstephens@outlook.com',
          recepUsername: 'username',
          recepPassword: 'password',
          recepPhone: '07665123988',
+         surgery: surgery
+        ).save()
+
+
+        def receptionist2 = new Receptionist(
+         recepName: 'Casey Kelly',
+         recepEmail: 'caseykelly@gmail.com',
+         recepUsername: 'ckelly',
+         recepPassword: 'password',
+         recepPhone: '07118765345',
          surgery: surgery
         ).save()
 
@@ -55,11 +99,30 @@ class BootStrap {
          surgery: surgery
         ).save()
 
+        def nurse2 = new Nurse(
+	 nurseName: 'Micheal Wright',
+         qualifications: 'Registered General Nurse',
+         nurseEmail: 'mwright@outlook.com',
+         nurseOffice: 'N-35',
+         nursePhone: '07398261297',
+         surgery: surgery
+        ).save()
+
         def appointment = new Appointment(
 	 appDate: '2023/02/12',
          appTime: '17:00',
 	 appDuration: 30,
 	 roomNumber: 'A-45',
+	 surgery: surgery,
+	 doctor: doctor,
+	 patient: patient
+	).save()
+
+        def appointment2 = new Appointment(
+	 appDate: '2023/03/15',
+         appTime: '10:30',
+	 appDuration: 60,
+	 roomNumber: 'A-50',
 	 surgery: surgery,
 	 doctor: doctor,
 	 patient: patient
@@ -75,7 +138,20 @@ class BootStrap {
 	 patientPaying: true,
 	 doctor: doctor,
          patient: patient
-	).save()*/
+	).save()
+
+        def prescription2 = new Prescription(
+	 pharmacyName: 'London Road Pharmacy',
+	 prescriptionNumber: 2289,
+	 medicine: 'Paracetamol',
+	 daysSupply: 10,
+	 totalCost: 1.99,
+	 dateIssued: new Date(2023 - 1200, 1, 13),
+	 patientPaying: true,
+	 doctor: doctor,
+         patient: patient
+	).save()
+
     }
     def destroy = {
     }
